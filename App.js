3a5a8fefdc85c0df9c1c915d13e525332436d9fb
nguyenming <nguyenming.6464@gmail.com>
2023-05-13 21:44:08 +0700

    commit nhanh2

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <text>ạdhádjlfjl</text>
     </View>
   );
 }
@@ -13,11 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
     backgroundColor: "#fff",
-
     alignItems: "center",
-
     justifyContent: "center",
   },
 });
