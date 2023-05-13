9a37d089600a0d1ea169afc246580d174066e4dd
nguyenming <nguyenming.6464@gmail.com>
2023-05-13 21:46:22 +0700

    xuongca nhanh3

--- App.js
+++ App.js
@@ -5,7 +5,13 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <text>ạdhádjlfjl</text>
+      <text>ạdhádjlfjl</text>
+      <text>ạdhádjlfjl</text>
       <StatusBar style="auto" />
+      <text>ạdhádjlfjl</text>
+      <text>ạdhádjlfjl</text>
+      <text>ạdhádjlfjl</text>
       <text>ạdhádjlfjl</text>
     </View>
   );
